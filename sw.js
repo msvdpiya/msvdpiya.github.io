c4ae625b2bb638eb0ce6dbf9ab9e8c3a9803cad4
Priya Darshini <msvdpriya@gmail.com>
2018-05-15 15:16:51 +0530

    Removing width

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
-var CACHE_STATIC_NAME = 'static-v3';
+var CACHE_STATIC_NAME = 'static-v33';
 var CACHE_DYNAMIC_NAME = 'dynamic-v7';
 var STATIC_FILES = [
   '/',
